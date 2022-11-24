e88db31c27a93aa4608a7b1cfc7d6e219e7e6cf2
dikiogres <dikiogres@gmail.com>
2022-11-24 16:03:57 +0700

    feat: Scene constructor

--- pages/lib/SceneInit.ts
+++ pages/lib/SceneInit.ts
@@ -3,5 +3,21 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Stats from "three/examples/jsm/libs/stats.module";
 
 export default class SceneInit {
+    
+    fov: number;
+    camera = new THREE.PerspectiveCamera;
+    scene = new THREE.Scene;
+    stats: number;
+    controls: string;
+    renderer = new THREE.WebGLRenderer;
 
+
+    constructor(fov = 36, camera:any, scene:any, stats:number, controls:string, renderer:any) {
+        this.fov = fov;
+        this.scene = scene;
+        this.stats = stats;
+        this.camera = camera;
+        this.controls = controls;
+        this.renderer = renderer;
+      }
 }
