8f6a06b13aef7332767e3b01915c0be266e56eb3
dikiogres <dikiogres@gmail.com>
2022-11-24 15:54:00 +0700

    feat: getMesh function

--- pages/lib/Planet.ts
+++ pages/lib/Planet.ts
@@ -1,20 +1,26 @@
 import * as THREE from "three";
 
-interface Props { 
-    radius: any;
-    positionX: any;
-    textureFile: any;
-}
-
 export default class Planet {
-    radius: Props;
-    positionX: Props;
-    textureFile: Props;
+    radius: number;
+    positionX: number;
+    textureFile: string;
+    mesh: any;
 
-    constructor(radius:Props, positionX:Props, textureFile:Props) {
+    constructor(radius:number, positionX:number, textureFile:string) {
         this.radius = radius;
         this.positionX = positionX;
         this.textureFile = textureFile;
     }
+
+    getMesh() {
+        if (this.mesh === undefined || this.mesh === null) {
+          const geometry = new THREE.SphereGeometry(this.radius);
+          const texture = new THREE.TextureLoader().load(this.textureFile);
+          const material = new THREE.MeshBasicMaterial({ map: texture });
+          this.mesh = new THREE.Mesh(geometry, material);
+          this.mesh.position.x += this.positionX;
+        }
+        return this.mesh;
+      }
 }
 
