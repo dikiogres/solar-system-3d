ff6c594cd3f51b414c80896e4447ab54a8cb03ce
dikiogres <dikiogres@gmail.com>
2022-11-24 15:57:38 +0700

    feat: Rotation constructor

--- pages/lib/Rotation.ts
+++ pages/lib/Rotation.ts
@@ -2,5 +2,18 @@ import * as THREE from "three";
 
 export default class Rotation {
 
+    planetMesh: any;
+    planetPositionX: any;
+    position: any;
+    y: number;
+    z: number;
+    showRotation: boolean;
+
+    constructor(planetMesh: any, showRotation = false) {
+        this.planetPositionX = planetMesh.position.x;
+        this.y = 0.25;
+        this.z = 0.25;
+        this.showRotation = showRotation;
+      }
 }
 
