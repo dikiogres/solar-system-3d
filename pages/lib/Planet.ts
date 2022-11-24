1ee6dd2b5c720f8a7c44069f272b13d194380081
dikiogres <dikiogres@gmail.com>
2022-11-24 15:50:24 +0700

    feat: Planet constructor

--- pages/lib/Planet.ts
+++ pages/lib/Planet.ts
@@ -1,6 +1,20 @@
 import * as THREE from "three";
 
-export default class Planet {
+interface Props { 
+    radius: any;
+    positionX: any;
+    textureFile: any;
+}
 
+export default class Planet {
+    radius: Props;
+    positionX: Props;
+    textureFile: Props;
+
+    constructor(radius:Props, positionX:Props, textureFile:Props) {
+        this.radius = radius;
+        this.positionX = positionX;
+        this.textureFile = textureFile;
+    }
 }
 
