9389b99cbfca05c5ad970fb3e7de9fcbb94b0d12
dikiogres <dikiogres@gmail.com>
2022-11-24 17:40:57 +0700

    feat: add solat system mesh GUI asynchronously

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,56 +15,64 @@ export default function Home() {
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
-    //  TODO: Understand this code later.
-    let test = new SceneInit();
-    test.initScene();
-    test.animate();
+    (
+      async ()=>{
+        //  TODO: Understand this code later.
+        let test = new SceneInit();
+        test.initScene();
+        test.animate();
 
-    const sunGeometry = new THREE.SphereGeometry(8);
-    const sunTexture = new THREE.TextureLoader().load("sun.jpeg");
-    const sunMaterial = new THREE.MeshBasicMaterial({ map: sunTexture });
-    const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
-    const solarSystem = new THREE.Group();
-    solarSystem.add(sunMesh);
-    test.scene.add(solarSystem);
+        const sunGeometry = new THREE.SphereGeometry(8);
+        const sunTexture = new THREE.TextureLoader().load("sun.jpeg");
+        const sunMaterial = new THREE.MeshBasicMaterial({ map: sunTexture });
+        const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
+        const solarSystem = new THREE.Group();
+        solarSystem.add(sunMesh);
+        test.scene.add(solarSystem);
 
-    const mercury = new Planet(2, 16, "mercury.png");
-    const mercuryMesh = mercury.getMesh();
-    let mercurySystem = new THREE.Group();
-    mercurySystem.add(mercuryMesh);
+        const mercury = new Planet(2, 16, "mercury.png");
+        const mercuryMesh = mercury.getMesh();
+        let mercurySystem = new THREE.Group();
+        mercurySystem.add(mercuryMesh);
 
-    const venus = new Planet(3, 32, "venus.jpeg");
-    const venusMesh = venus.getMesh();
-    let venusSystem = new THREE.Group();
-    venusSystem.add(venusMesh);
+        const venus = new Planet(3, 32, "venus.jpeg");
+        const venusMesh = venus.getMesh();
+        let venusSystem = new THREE.Group();
+        venusSystem.add(venusMesh);
 
-    const earth = new Planet(4, 48, "earth.jpeg");
-    const earthMesh = earth.getMesh();
-    let earthSystem = new THREE.Group();
-    earthSystem.add(earthMesh);
+        const earth = new Planet(4, 48, "earth.jpeg");
+        const earthMesh = earth.getMesh();
+        let earthSystem = new THREE.Group();
+        earthSystem.add(earthMesh);
 
-    const mars = new Planet(3, 64, "mars.jpeg");
-    const marsMesh = mars.getMesh();
-    let marsSystem = new THREE.Group();
-    marsSystem.add(marsMesh);
+        const mars = new Planet(3, 64, "mars.jpeg");
+        const marsMesh = mars.getMesh();
+        let marsSystem = new THREE.Group();
+        marsSystem.add(marsMesh);
 
-    solarSystem.add(mercurySystem, venusSystem, earthSystem, marsSystem);
+        solarSystem.add(mercurySystem, venusSystem, earthSystem, marsSystem);
 
-    const mercuryRotation = new Rotation(mercuryMesh);
-    const mercuryRotationMesh = mercuryRotation.getMesh();
-    mercurySystem.add(mercuryRotationMesh);
-    const venusRotation = new Rotation(venusMesh);
-    const venusRotationMesh = venusRotation.getMesh();
-    venusSystem.add(venusRotationMesh);
-    const earthRotation = new Rotation(earthMesh);
-    const earthRotationMesh = earthRotation.getMesh();
-    earthSystem.add(earthRotationMesh);
-    const marsRotation = new Rotation(marsMesh);
-    const marsRotationMesh = marsRotation.getMesh();
-    marsSystem.add(marsRotationMesh);
+        const mercuryRotation = new Rotation(mercuryMesh);
+        const mercuryRotationMesh = mercuryRotation.getMesh();
+        mercurySystem.add(mercuryRotationMesh);
+        const venusRotation = new Rotation(venusMesh);
+        const venusRotationMesh = venusRotation.getMesh();
+        venusSystem.add(venusRotationMesh);
+        const earthRotation = new Rotation(earthMesh);
+        const earthRotationMesh = earthRotation.getMesh();
+        earthSystem.add(earthRotationMesh);
+        const marsRotation = new Rotation(marsMesh);
+        const marsRotationMesh = marsRotation.getMesh();
+        marsSystem.add(marsRotationMesh);
 
-    
-
+        await initGui();
+        const solarSystemGui = gui.addFolder("solar system");
+        solarSystemGui.add(mercuryRotationMesh, "visible").name("mercury").listen();
+        solarSystemGui.add(venusRotationMesh, "visible").name("venus").listen();
+        solarSystemGui.add(earthRotationMesh, "visible").name("earth").listen();
+        solarSystemGui.add(marsRotationMesh, "visible").name("mars").listen();
+      }
+    )
   }, []);  
 
   return (
